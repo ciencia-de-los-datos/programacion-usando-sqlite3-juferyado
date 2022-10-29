72daa3c21d994a73527eb63e9128e2d1db5dc376
juferyado <111617160+juferyado@users.noreply.github.com>
2022-10-28 20:22:30 -0500

    Update pregunta_08.sql

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -41,4 +41,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-  SELECT strftime('%Y',c23), AVG(c21) AS 'avg(c21)' FROM tbl2 GROUP BY strftime('%Y',c23)
+  SELECT SUBSTR(c23, 1,4) YEAR , avg(c21) FROM tbl2 GROUP BY SUBSTR(c23, 1,4)
